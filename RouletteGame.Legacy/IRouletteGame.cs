e05c95df3334f29448c6459e1729c00c8aaa491a
Maiken Guldberg <maiken.g@fibermail.dk>
2018-02-20 09:36:34 +0100

    Init 2

---
+++ RouletteGame.Legacy/IRouletteGame.cs
@@ -0,0 +1,11 @@
+﻿namespace RouletteGame.Legacy
+{
+    public interface IRouletteGame
+    {
+        void CloseBets();
+        void OpenBets();
+        void PayUp();
+        void PlaceBet(Bet bet);
+        void SpinRoulette();
+    }
+}
